233e620c05e2d19c08ad968b41b5519731b3d2fc
Farhan Hanif <38966867+Chillrend@users.noreply.github.com>
2023-02-01 15:50:32 +0700

    added phpdoc for function render in SSOMigrationController

--- app/Http/Controllers/SSOMigrationController.php
+++ app/Http/Controllers/SSOMigrationController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class SSOMigrationController extends Controller
 {
 
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     *
+     * returns the first step form view
+     */
     public function render(){
         return view('migration_form');
     }
