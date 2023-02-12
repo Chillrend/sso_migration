88e038308354822b51fd77c7e59edc3896f49df9
Farhan Hanif <madjavacoder@gmail.com>
2023-02-12 22:12:33 +0700

    revised the import method for the org units

---
+++ node_scripts/base_group.js
@@ -0,0 +1,73 @@
+
+export const groups = {
+    groups: [
+        {
+            name: "Mahasiswa",
+            attributes: {
+                description: "Group Untuk Akun Mahasiswa",
+                version: "1.0",
+                short_unit_name: "Mhsw",
+                unit_name: "Mahasiswa"
+            },
+            sub_groups: []
+        },
+        {
+            name: "Alumni",
+            attributes: {
+                description: "Group Untuk Akun Alumni",
+                version: "1.0",
+                short_unit_name: "Alumni",
+                unit_name: "Alumni"
+            },
+            sub_groups: []
+        },
+        {
+            name: "Dosen",
+            attributes: {
+                description: "Group Untuk Akun Dosen",
+                version: "1.0",
+                short_unit_name: "Dosen",
+                unit_name: "Dosen"
+            },
+            sub_groups: []
+        },
+        {
+            name: "Staf",
+            attributes: {
+                description: "Group Untuk Akun Staf",
+                version: "1.0",
+                short_unit_name: "Staf",
+                unit_name: "Staf"
+            },
+            sub_groups: []
+        },
+        {
+            name: "Pimpinan",
+            attributes: {
+                description: "Group Untuk Akun Pimpinan dan Staff nya",
+                version: "1.0",
+                short_unit_name: "Pimpinan",
+                unit_name: "Pimpinan"
+            },
+            sub_groups: []
+        },
+        {
+            name: "Tamu",
+            attributes: {
+                description: "Group Untuk Tamu",
+                version: "1.0",
+                short_unit_name: "Tamu",
+                unit_name: "Tamu"
+            }
+        },
+        {
+            name: "Administrator",
+            attributes: {
+                description: "Group Untuk Akun Administrator Unit TI Politeknik Negeri Jakarta",
+                version: "1.0",
+                short_unit_name: "Admin",
+                unit_name: "Admin"
+            }
+        },
+    ]
+};
--- node_scripts/generate_org_unit.js
+++ node_scripts/generate_org_unit.js
@@ -1,9 +1,34 @@
+import {groups as base_group} from "./base_group";
 
 const fs = require('fs');
 const csvToObj = require('csv-to-js-parser').csvToObj;
 
 const data = fs.readFileSync('jurusan_unit.csv').toString();
 
-let jurusan = csvToObj(data);
+let org_units = csvToObj(data);
 
-console.log(JSON.stringify(jurusan));
+let mahasiswa_groups = base_group.groups[0];
+let alumni_groups = base_group.groups[1];
+let dosen_groups = base_group.groups[2];
+let staf_groups = base_group.groups[3];
+let pimpinan_groups = base_group.groups[4];
+org_units.forEach((unit) => {
+    // Mahasiswa
+    if(unit.jurusan === "TRUE"){
+        let jurusan = {
+            name: unit.name,
+            attributes: {
+                description: (unit.description, "Mahasiswa"),
+                version: "1.0",
+                short_unit_name: unit.short_unit_name,
+                unit_name: unit.short_unit_name
+            },
+        }
+
+        mahasiswa_groups.sub_groups.push(jurusan);
+    }
+})
+
+
+
+console.log(JSON.stringify(mahasiswa_groups));
